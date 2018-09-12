b7c9438072109ee86f75941164cbb370a6986665
Derek Scharton <derek@aplos.com>
2018-09-12 13:08:28 -0700

    Add truncateParts to separate email and domain

--- src/substring/SubstringEmail.java
+++ src/substring/SubstringEmail.java
@@ -1,17 +1,51 @@
 package substring;
 
-
 public class SubstringEmail {
 
 	public String echo(String input) {
 		return input;
 	}
-	
+
 	public String truncateWhole(String emailInput, Integer truncateLength) {
 		if (emailInput.length() >= truncateLength) {
 			return emailInput.substring(0, truncateLength.intValue());
 		}
 		return emailInput;
-		
+
+	}
+
+	/**
+	 * Decompose an email by the @ separator. Each length is what is the amount of the
+	 * email address to return.
+	 * <p>
+	 * Example (1, 6):
+	 * 
+	 * <pre>
+	 * doug@mailinator.com -> d@mailin
+	 * </pre>
+	 * 
+	 * @param emailInput
+	 * @param emailLength
+	 * @param domainLength
+	 * @return
+	 */
+	public String truncateParts(String emailInput, Integer emailLength, Integer domainLength) {
+		String[] emailInputParts = emailInput.split("@");
+		if (emailInputParts.length == 1) {
+			if (emailInputParts[0].length() > emailLength + domainLength) {
+				return emailInput.substring(0, emailLength + domainLength);
+			}
+			return emailInput;
+		}
+
+		String emailPart = emailInputParts[0].length() > emailLength ? emailInputParts[0].substring(0, emailLength) : emailInputParts[0];
+		String domainPart = emailInputParts[1].length() > domainLength ? emailInputParts[1].substring(0, domainLength) : emailInputParts[1];
+		return emailPart + "@" + domainPart;
+		// for (String p : emailInputParts) {
+		//
+		//
+		// System.out.println(p);
+		// }
+
 	}
 }
--- src/substring/SubstringEmailTest.java
+++ src/substring/SubstringEmailTest.java
@@ -14,19 +14,18 @@ class SubstringEmailTest {
 
 	@Test
 	void testEcho() {
-		String echoInput = "derek@aplos.com";
+		String echoInput = "derek@mailinator.com";
 		
 		SubstringEmail email = new SubstringEmail();
 		String echoOutput = email.echo(echoInput);
 		assertEquals(echoInput, echoOutput);
 	}
-
 	
 	@Test
 	void testTruncateWhole() {
 		final int truncateLength = 10;
 		Map<String,String> map = new HashMap<String, String>();
-		map.put("derek@aplos.com", "derek@aplo");
+		map.put("derek@mailinator.com", "derek@mail");
 		map.put("d@x.com", "d@x.com");
 		
 		Set<Entry<String,String>> entrySet = map.entrySet();
@@ -35,5 +34,24 @@ class SubstringEmailTest {
 			String truncatedEmail = email.truncateWhole(entry.getKey(), Integer.valueOf(truncateLength));
 			assertEquals(truncatedEmail, entry.getValue());
 		}
+	}
+	
+	@Test
+	void testTruncateParts() {
+		final int emailLen = 1;
+		final int domainLen = 6;
+		Map<String,String> map = new HashMap<String, String>();
+		map.put("doug@mailinator.com", "d@mailin");
+		map.put("doug@m.com", "d@m.com");
+		map.put("dougatmdotcom", "dougatm");
+		
+		Set<Entry<String,String>> entrySet = map.entrySet();
+		for (Entry<String,String> entry : entrySet) {
+			SubstringEmail email = new SubstringEmail();
+			String truncatedEmail = email.truncateParts(entry.getKey(), emailLen, domainLen);
+//			System.out.println(truncatedEmail);
+			assertEquals(truncatedEmail, entry.getValue());
+		}
+		
 	}
 }
