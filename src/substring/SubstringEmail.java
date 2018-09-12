1371c6b88531a783d3fd732f8e79daaa796d21a8
Derek Scharton <derek@aplos.com>
2018-09-12 12:48:33 -0700

    Add truncateWhole for string eval without email decomposition

--- src/substring/SubstringEmail.java
+++ src/substring/SubstringEmail.java
@@ -6,4 +6,12 @@ public class SubstringEmail {
 	public String echo(String input) {
 		return input;
 	}
+	
+	public String truncateWhole(String emailInput, Integer truncateLength) {
+		if (emailInput.length() >= truncateLength) {
+			return emailInput.substring(0, truncateLength.intValue());
+		}
+		return emailInput;
+		
+	}
 }
--- src/substring/SubstringEmailTest.java
+++ src/substring/SubstringEmailTest.java
@@ -2,6 +2,11 @@ package substring;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 
@@ -14,7 +19,21 @@ class SubstringEmailTest {
 		SubstringEmail email = new SubstringEmail();
 		String echoOutput = email.echo(echoInput);
 		assertEquals(echoInput, echoOutput);
-
 	}
 
+	
+	@Test
+	void testTruncateWhole() {
+		final int truncateLength = 10;
+		Map<String,String> map = new HashMap<String, String>();
+		map.put("derek@aplos.com", "derek@aplo");
+		map.put("d@x.com", "d@x.com");
+		
+		Set<Entry<String,String>> entrySet = map.entrySet();
+		for (Entry<String, String> entry : entrySet) {
+			SubstringEmail email = new SubstringEmail();
+			String truncatedEmail = email.truncateWhole(entry.getKey(), Integer.valueOf(truncateLength));
+			assertEquals(truncatedEmail, entry.getValue());
+		}
+	}
 }
